a6e78c5d64dfb622077e1fe7d83021a8142d7f88
Sweb7xx <ydvlreversing@gmail.com>
2025-04-13 14:43:33 -0400

    Create SwebScript_Arsenal.lua

---
+++ Games/SwebScript_Arsenal.lua
@@ -0,0 +1,9 @@
+-- SwebScript Arsenal
+print("SwebScript Arsenal loaded successfully!")
+
+-- Your Arsenal script code will go here
+local notification = Instance.new("Message")
+notification.Text = "SwebScript Arsenal loaded successfully!"
+notification.Parent = game.Workspace
+wait(3)
+notification:Destroy()

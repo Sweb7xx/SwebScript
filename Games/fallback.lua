2ff700f30a04c5ec7d86a6e233d3de24ab695f25
Sweb7xx <ydvlreversing@gmail.com>
2025-04-13 14:45:26 -0400

    Create fallback.lua

---
+++ Games/fallback.lua
@@ -0,0 +1,7 @@
+-- Fallback script
+print("SwebScript fallback loaded")
+local notification = Instance.new("Message")
+notification.Text = "SwebScript loaded in fallback mode"
+notification.Parent = game.Workspace
+wait(3)
+notification:Destroy()

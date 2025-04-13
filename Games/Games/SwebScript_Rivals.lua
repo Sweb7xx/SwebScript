c7512785184146b97e9e3d5c8c0495d7c338c849
Sweb7xx <ydvlreversing@gmail.com>
2025-04-13 14:43:50 -0400

    Create SwebScript_Rivals.lua

---
+++ Games/Games/SwebScript_Rivals.lua
@@ -0,0 +1,9 @@
+-- SwebScript Rivals
+print("SwebScript Rivals loaded successfully!")
+
+-- Your Rivals script code will go here
+local notification = Instance.new("Message")
+notification.Text = "SwebScript Rivals loaded successfully!"
+notification.Parent = game.Workspace
+wait(3)
+notification:Destroy()
